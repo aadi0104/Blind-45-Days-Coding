032bba33c01383cf18267e90fc454e2236fff583
aadi0104 <aadill0104@gmail.com>
2025-04-07 15:39:06 +0530

    Binary Tree Height

---
+++ src/Binary_Tree_1/Binary_Tree_Height.java
@@ -0,0 +1,31 @@
+package Binary_Tree_1;
+
+import java.util.Scanner;
+
+import Binary_Tree_1.Implementation_Of_BT_Using_Preorder.Node;
+
+public class Binary_Tree_Height {
+
+	class Node {
+		int val;
+		Node left;
+		Node right;
+	}
+
+	private Node root;
+	
+//	Height
+	public int Height() {
+		return Height(root);
+	}
+
+	public int Height(Node node) {
+		if (node == null) {
+			return -1;
+		}
+		int left = Height(node.left);
+		int right = Height(node.right);
+		return 1 + Math.max(left, right);
+	}
+	
+}
