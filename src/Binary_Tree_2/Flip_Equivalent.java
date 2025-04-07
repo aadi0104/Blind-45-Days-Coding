ef5c33329c2dc510d3410815b040b25feb1c355f
aadi0104 <aadill0104@gmail.com>
2025-04-07 20:23:41 +0530

    Flip Equivalent

---
+++ src/Binary_Tree_2/Flip_Equivalent.java
@@ -0,0 +1,48 @@
+package Binary_Tree_2;
+
+// 101. Symmetric Tree
+public class Flip_Equivalent {
+
+	public class TreeNode {
+		int val;
+		TreeNode left;
+		TreeNode right;
+
+		TreeNode() {
+		}
+
+		TreeNode(int val) {
+			this.val = val;
+		}
+
+		TreeNode(int val, TreeNode left, TreeNode right) {
+			this.val = val;
+			this.left = left;
+			this.right = right;
+		}
+	}
+
+	class Solution {
+
+		public boolean flipEquiv(TreeNode root1, TreeNode root2) {
+			return flipEqual(root1, root2);
+		}
+
+		public boolean flipEqual(TreeNode node1, TreeNode node2) {
+			if (node1 == null && node2 == null) {
+				return true;
+			}
+			if (node1 == null || node2 == null) {
+				return false;
+			}
+			if (node1.val == node2.val) {
+				return true;
+			}
+			boolean flip = flipEqual(node1.left, node2.right) && flipEqual(node1.right, node2.left);
+			boolean equal = flipEqual(node1.left, node2.left) && flipEqual(node1.right, node2.right);
+			return flip || equal;
+		}
+
+	}
+
+}
