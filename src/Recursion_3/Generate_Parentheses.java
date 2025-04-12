d0e1965dfa0ea101698d2789478b0034581b4d81
aadi0104 <aadill0104@gmail.com>
2025-04-12 20:48:13 +0530

    Generate Parentheses

--- src/Recursion_3/Generate_Parentheses.java
+++ src/Recursion_3/Generate_Parentheses.java
@@ -9,20 +9,25 @@ public class Generate_Parentheses {
 
 		public List<String> generateParenthesis(int n) {
 			List<String> ans = new ArrayList<>();
-			generate(n, ans, 0, 0, "");
+			StringBuilder sb = new StringBuilder();
+			generate(n, ans, 0, 0, sb);
 			return ans;
 		}
-	
-		public void generate(int n, List<String> ans, int open, int close, String temp) {
+
+		public void generate(int n, List<String> ans, int open, int close, StringBuilder temp) {
 			if (open == n && close == n) {
-				ans.add(temp);
+				ans.add(temp.toString());
 				return;
 			}
 			if (open > n || close > open) {
 				return;
 			}
-			generate(n, ans, open + 1, close, temp + "(");
-			generate(n, ans, open, close + 1, temp + ")");
+			temp.append("(");
+			generate(n, ans, open + 1, close, temp);
+			temp.deleteCharAt(temp.length() - 1);
+			temp.append(")");
+			generate(n, ans, open, close + 1, temp);
+			temp.deleteCharAt(temp.length() - 1);
 		}
 
 	}
