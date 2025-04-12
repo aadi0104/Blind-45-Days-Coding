01d396a530f37a6e095e30772015842c948bc3b7
aadi0104 <aadill0104@gmail.com>
2025-04-12 15:23:16 +0530

    Power

---
+++ src/Recursion_1/Power.java
@@ -0,0 +1,19 @@
+package Recursion_1;
+
+public class Power {
+
+	public static void main(String[] args) {
+		int num = 5;
+		int power = 2;
+		int ans = pow(num, power);
+		System.out.print(ans);
+	}
+	
+	public static int pow(int num, int power) {
+		if(power == 1) {
+			return num;
+		}
+		return num * pow(num, power - 1);
+	}
+
+}
