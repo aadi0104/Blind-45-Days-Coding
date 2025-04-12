5dad9d81d4bd26e8d958c406e42f4781a4d54250
aadi0104 <aadill0104@gmail.com>
2025-04-12 12:56:43 +0530

    Dynamic Stacks Implementation Using Array

---
+++ src/Stack_And_Queue_1/Stack_Implementation.java
@@ -0,0 +1,56 @@
+package Stack_And_Queue_1;
+
+public class Stack_Implementation {
+
+	private int[] stack;
+	private int idx = -1;
+
+	public Stack_Implementation() {
+		stack = new int[5];
+	}
+
+	public boolean isFull() {
+		return idx == stack.length;
+	}
+
+	public boolean isEmpty() {
+		return idx == -1;
+	}
+
+	public void createStack(int len) {
+		int[] new_stack = new int[len];
+		for (int i = 0; i < idx; i++) {
+			new_stack[i] = stack[i];
+		}
+		stack = new_stack;
+	}
+
+	public void push(int item) {
+		if (isFull()) {
+			stack[idx + 1] = item;
+		} else {
+			createStack(stack.length * 2);
+			stack[idx + 1] = item;
+		}
+		idx++;
+	}
+
+	public int peek() throws Exception {
+		if (isEmpty()) {
+			throw new Exception("Underflow condition!");
+		}
+		return stack[idx];
+	}
+
+	public int pop() throws Exception {
+		if (isEmpty()) {
+			throw new Exception("Underflow condition!");
+		}
+		return stack[idx--];
+	}
+
+	public int size() {
+		return idx + 1;
+	}
+
+}
