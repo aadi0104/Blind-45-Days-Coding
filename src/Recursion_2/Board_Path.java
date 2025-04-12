d28c9d2df3bc239dcd3d0ed92aef88f68ed370f6
aadi0104 <aadill0104@gmail.com>
2025-04-12 18:33:00 +0530

    Board Path (Print And COunt)

---
+++ src/Recursion_2/Board_Path.java
@@ -0,0 +1,27 @@
+package Recursion_2;
+
+public class Board_Path {
+
+	public static void main(String[] args) {
+		int dice = 3;
+		int place = 4;
+		int ans = path(dice, place, "", 0);
+		System.out.print(ans);
+	}
+
+	public static int path(int dice, int place, String path, int pathSum) {
+		if (pathSum > place) {
+			return 0;
+		}
+		if (pathSum == place) {
+			System.out.println(path);
+			return 1;
+		}
+		int ans = 0;
+		for (int i = 1; i <= dice; i++) {
+			ans += path(dice, place, path + i, pathSum + i);
+		}
+		return ans;
+	}
+
+}
