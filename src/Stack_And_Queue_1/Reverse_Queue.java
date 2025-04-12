5776592bb72f57211440b401c4d24a5203768ef5
aadi0104 <aadill0104@gmail.com>
2025-04-12 13:56:20 +0530

    Reverse Queue Using Recursion

---
+++ src/Stack_And_Queue_1/Reverse_Queue.java
@@ -0,0 +1,27 @@
+package Stack_And_Queue_1;
+
+import java.util.*;
+
+public class Reverse_Queue {
+
+	public static void main(String[] args) {
+		int[] queue = { 1, 3, 5, 4, 7, 6, 8 };
+		Queue<Integer> q = new LinkedList<>();
+		for (int i = 0; i < queue.length; i++) {
+			q.add(queue[i]);
+		}
+		System.out.println(q);
+		reverse(q);
+		System.out.println(q);
+	}
+
+	public static void reverse(Queue<Integer> q) {
+		if(q.isEmpty()) {
+			return;
+		}
+		int val = q.poll();
+		reverse(q);
+		q.add(val);
+	}
+
+}
