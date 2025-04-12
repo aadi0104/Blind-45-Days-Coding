54a58aa1698b5d26be556835e1a398434005b6a4
aadi0104 <aadill0104@gmail.com>
2025-04-12 20:12:29 +0530

    Lexicographical Numbers

---
+++ src/Recursion_3/Lexico_Counting.java
@@ -0,0 +1,34 @@
+package Recursion_3;
+
+import java.util.*;
+
+// 386. Lexicographical Numbers
+public class Lexico_Counting {
+
+	class Solution {
+
+		public List<Integer> lexicalOrder(int n) {
+			List<Integer> ans = new ArrayList<>();
+			lexico(n, ans, 0);
+			return ans;
+		}
+
+		public void lexico(int n, List<Integer> ans, int num) {
+	        if (num > n) {
+	            return;
+	        }
+	        if (num != 0) {
+	            ans.add(num);
+	        }
+	        int i = 0;
+	        if (num == 0) {
+	            i = 1;
+	        }
+	        for (; i <= 9; i++) {
+	            lexico(n, ans, num * 10 + i);
+	        }
+	    }
+
+	}
+
+}
