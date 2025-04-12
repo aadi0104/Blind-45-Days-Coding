c16620e69603613c7c6a4e6a04dcf2ffebe65da3
aadi0104 <aadill0104@gmail.com>
2025-04-12 23:41:25 +0530

    Combination Sum III

---
+++ src/Recursion_And_Backtracking_1/Combination_Sum_III.java
@@ -0,0 +1,36 @@
+package Recursion_And_Backtracking_1;
+
+import java.util.*;
+
+// 216. Combination Sum III
+public class Combination_Sum_III {
+
+	class Solution {
+
+		public List<List<Integer>> combinationSum3(int k, int n) {
+			List<List<Integer>> ans = new ArrayList<>();
+			combination(k, n, new ArrayList<>(), ans, 1);
+			return ans;
+		}
+
+		public void combination(int k, int n, List<Integer> temp, List<List<Integer>> ans, int idx) {
+			if (k == 0 && n == 0) {
+				ans.add(new ArrayList<>(temp));
+				return;
+			}
+			for (int i = idx; i <= 9; i++) {
+				if (k == 0) {
+					break;
+				}
+				if (i > n) {
+					break;
+				}
+				temp.add(i);
+				combination(k - 1, n - i, temp, ans, i + 1);
+				temp.remove(temp.size() - 1);
+			}
+		}
+
+	}
+
+}
