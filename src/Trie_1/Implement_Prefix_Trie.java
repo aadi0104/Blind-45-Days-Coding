79d479225b82cee8670421db224ed7600d399f9c
aadi0104 <aadill0104@gmail.com>
2025-04-16 22:17:20 +0530

    Implement Trie (Prefix Tree)

---
+++ src/Trie_1/Implement_Prefix_Trie.java
@@ -0,0 +1,67 @@
+package Trie_1;
+
+import java.util.HashMap;
+
+import Trie_1.Trie.Node;
+
+public class Implement_Prefix_Trie {
+
+	class Trie {
+
+		class Node {
+			char ch;
+			HashMap<Character, Node> child = new HashMap<>();
+			boolean isTerminal;
+		}
+
+		private Node root;
+
+		public Trie() {
+			root = new Node();
+		}
+
+		public void insert(String word) {
+			Node curr = root;
+			for (int i = 0; i < word.length(); i++) {
+				char ch = word.charAt(i);
+				if (curr.child.containsKey(ch)) {
+					curr = curr.child.get(ch);
+				} else {
+					Node nn = new Node();
+					nn.ch = ch;
+					curr.child.put(ch, nn);
+					curr = nn;
+				}
+			}
+			curr.isTerminal = true;
+		}
+
+		public boolean search(String word) {
+			Node curr = root;
+			for (int i = 0; i < word.length(); i++) {
+				char ch = word.charAt(i);
+				if (curr.child.containsKey(ch)) {
+					curr = curr.child.get(ch);
+				} else {
+					return false;
+				}
+			}
+			return curr.isTerminal;
+		}
+
+		public boolean startsWith(String prefix) {
+			Node curr = root;
+			for (int i = 0; i < prefix.length(); i++) {
+				char ch = prefix.charAt(i);
+				if (curr.child.containsKey(ch)) {
+					curr = curr.child.get(ch);
+				} else {
+					return false;
+				}
+			}
+			return true;
+		}
+		
+	}
+
+}
